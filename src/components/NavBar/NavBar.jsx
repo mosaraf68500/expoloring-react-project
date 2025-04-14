796b0483ad9ace948d5ba2a55b88cd687ed26f7f
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-14 23:13:56 +0600

    custom navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      {/* <ul className='flex'>
+      <li className='mr-10'><a href="./home">Home</a></li>
+      <li className='mr-10'><a href="./about">About</a></li>
+      <li className='mr-10'><a href="./services">Service</a></li>
+      <li className='mr-10'><a href="./blogs">Blogs</a></li>
+    </ul> */}
+    </nav>
+  );
+};
+
+export default NavBar;
